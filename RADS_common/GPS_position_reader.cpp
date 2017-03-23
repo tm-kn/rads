b299a729ac19561f92951ff3e9038036b1c41edd
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2017-03-23 20:23:38 +0000

    Add more realistic altitude generation

--- RADS_common/GPS_position_reader.cpp
+++ RADS_common/GPS_position_reader.cpp
@@ -17,7 +17,7 @@ namespace Readings {
     namespace GPS_position {
         float GPS_position_reader::last_lat = static_cast<float> (rand() / static_cast<float> (RAND_MAX / 180));
         float GPS_position_reader::last_lng = static_cast<float> (rand() / static_cast<float> (RAND_MAX / 180));
-        float GPS_position_reader::last_alt = static_cast<float> (rand() / static_cast<float> (RAND_MAX / 180));
+        float GPS_position_reader::last_alt = (static_cast<float> (rand() / static_cast<float> (RAND_MAX / 10000)) + 30000);
 
         GPS_position_reader::GPS_position_reader() {}
 
@@ -36,7 +36,13 @@ namespace Readings {
                 // Generate random decimal number from and add it to the previous readings.
                 random_lat += static_cast<float> (rand() / static_cast<float> (RAND_MAX/0.5)); // Add 0 - 0.5 for latitude
                 random_lng += static_cast<float> (rand() / static_cast<float> (RAND_MAX/0.5)); // Add 0 - 0.5 for longitude
-                random_alt += static_cast<float> (rand() / static_cast<float> (RAND_MAX/0.001));  // Add 0 - 0.001 for altitude
+               
+                if (i % 3 == 0) {
+                    random_alt += static_cast<float> (rand() / static_cast<float> (RAND_MAX/30));  // Add 0 - 30 for altitude
+                }
+                else {
+                    random_alt -= static_cast<float> (rand() / static_cast<float> (RAND_MAX / 100));  // Decrease altitude by 0 - 100
+                }
             }
 
             // Assign last readings to the static variables so they're kept for the next readings
